2d5e24200c97aef921bb74d6a2003b275c30765d
Fred Rueda <fredruedao@gmail.com>
2025-04-15 21:24:49 -0500

    fix autenticacion para listar

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,13 @@ from config.views_api import router  # 👈 el router central
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # 👈 unifica todos los endpoints
+    path('api/', include(router.urls)),
+    path('api/', include('usuarios.urls')),
+    path('api/', include('documentos.urls')),
+    path('api/', include('empleados.urls')),
+    path('api/', include('vinculaciones.urls')),
+    path('api/', include('historial.urls')),
+    path('api/', include('auditoria.urls')),
+    path('api/', include('catalogos.urls')),
+    path('api/', include('empresa.urls')),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from empleados.models import Employee
+from rest_framework.permissions import AllowAny
 from .serializers import EmployeeSerializer
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all().order_by('-id')
     serializer_class = EmployeeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
+    
